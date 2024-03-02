dbd52ecea58e42f0b861bd39723fa9dc7f1c12ee
Zioun <jionkhan0@gmail.com>
2024-03-02 11:25:06 +0600

    added

--- js/main.js
+++ js/main.js
@@ -30,21 +30,24 @@ const fetchDataByCategory = (categoryID) => {
     .then(({data}) => {
         cardContainer.innerHTML = "";
         data.forEach((categoryCard)=>{
-            
+            let verifiedImg = "";
+            if(categoryCard.authors[0].verified){
+                verifiedImg = `<img src="./image/verify.png" alt="verify"></img>`
+            }
             const card = document.createElement("div");
             card.classList.add("card", "card-compact", "w-[312px]", "bg-base-100", "m-auto");
             card.innerHTML = `
             <img class="rounded-xl h-[200px]"  src="${categoryCard.thumbnail}" alt="Shoes"/>
             <div class="flex gap-3 mt-5">
-                <div class="h-[60px] w-[60px]">
-                <img class="w-full h-10 rounded-full" src="${categoryCard.authors[0].profile_picture}" alt="profile">
+                <div class="h-[40px] w-[40px]">
+                    <img class="w-full h-10 rounded-full" src="${categoryCard.authors[0].profile_picture}" alt="profile">
                 </div>
                 <div>
                 <h1 class="font-bold text-[16px]">${categoryCard.title}</h1>
                 <div  class="flex items-center gap-2">
                     <h2>${categoryCard.authors[0].profile_name}</h2>
                     <div>
-                        <img src="image/verify.png" alt="verify">
+                        ${verifiedImg}
                     </div>
                 </div>
                 <h3>${categoryCard.others.views} <span>Views</span></h3>
