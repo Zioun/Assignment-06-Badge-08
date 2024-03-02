c93a2a6834f8d8ae710b582fcc3486a8f85bd837
Zioun <jionkhan0@gmail.com>
2024-03-02 11:05:03 +0600

    all default show

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 const btnContainer = document.getElementById("btn-container");
+const cardContainer = document.getElementById("card-container");
+let selectedCategory = 1000;
+
+
+
 const fetchCategory = () =>{
     const url = "https://openapi.programming-hero.com/api/videos/categories";
     fetch(url)
@@ -6,10 +11,51 @@ const fetchCategory = () =>{
     .then(({data}) => {
         data.forEach((card)=>{
             const category = document.createElement("button");
-            category.classList.add("btn", "btn-sm", "ml-2", "mr-2")
+            category.classList.add("btn", "btn-sm", "ml-2", "mr-2", "text-[#252525]", "bg-[#D3D3D3]", "hover:bg-[#ff304b]", "hover:text-white", "font-semibold")
             category.innerText = card.category;
-            btnContainer.append(category)
+            category.addEventListener('click', () => fetchDataByCategory(card.category_id))
+            btnContainer.append(category);
+            console.log(data)
         })
     }) 
 }
+
+
+const fetchDataByCategory = (categoryID) => {
+    selectedCategory = categoryID
+    console.log(categoryID);
+    const url = `https://openapi.programming-hero.com/api/videos/category/${categoryID}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then(({data}) => {
+        cardContainer.innerHTML = "";
+        data.forEach((categoryCard)=>{
+            
+            const card = document.createElement("div");
+            card.classList.add("card", "card-compact", "w-[312px]", "bg-base-100", "m-auto");
+            card.innerHTML = `
+            <img class="rounded-xl h-[200px]"  src="${categoryCard.thumbnail}" alt="Shoes"/>
+            <div class="flex gap-3 mt-5">
+                <div class="h-[60px] w-[60px]">
+                <img class="w-full h-10 rounded-full" src="${categoryCard.authors[0].profile_picture}" alt="profile">
+                </div>
+                <div>
+                <h1 class="font-bold text-[16px]">${categoryCard.title}</h1>
+                <div  class="flex items-center gap-2">
+                    <h2>${categoryCard.authors[0].profile_name}</h2>
+                    <div>
+                        <img src="image/verify.png" alt="verify">
+                    </div>
+                </div>
+                <h3>${categoryCard.others.views} <span>Views</span></h3>
+                </div>
+            </div>
+            `;
+            cardContainer.appendChild(card);
+            console.log(categoryCard)
+        });
+        
+    })
+}
 fetchCategory();
+fetchDataByCategory(selectedCategory);
